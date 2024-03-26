1bedb48396a9f1075a043bf7dc4ed0e85d6550cf
sabelosiba <dlmsab007@myuct.ac.za>
2024-03-26 03:01:11 -0700

    week 3 content.py

---
+++ week3/dd_gui.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import  ttk
+
+class DailyDigestGUI:
+
+    def __init__(self, root):
+        pass # build  GUI here
+
+
+
+if  __name__ == "__main__":
+    root  = Tk()
+    daily_digest = DailyDigestGUI(root)
+    root.mainloop()
